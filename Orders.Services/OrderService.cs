5c788212338dbaa071ddb60d359d7ce9a87c0cf4
rawanyoussef <rawanyoussef@LAPTOP-3V5FQ72L>
2025-12-14 22:35:00 +0200

    HandlingErorr

--- Orders.Services/OrderService.cs
+++ Orders.Services/OrderService.cs
@@ -5,6 +5,8 @@ using AutoMapper;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
 
 namespace Orders.Services
 {
@@ -13,55 +15,100 @@ namespace Orders.Services
         private readonly IOrderRepo _orderRepo;
         private readonly ICacheService _cache;
         private readonly IMapper _mapper;
+        private readonly ILogger<OrderService> _logger;
 
-        public OrderService(IOrderRepo orderRepo, ICacheService cache, IMapper mapper)
+        public OrderService(IOrderRepo orderRepo, ICacheService cache, IMapper mapper ,ILogger<OrderService> logger)
         {
             _orderRepo = orderRepo;
             _cache = cache;
             _mapper = mapper;
+            _logger = logger;
         }
 
         public async Task AddOrderAsync(OrderCreateDto orderDto)
         {
-            var order = _mapper.Map<Order>(orderDto);
-            order.Id = Guid.NewGuid();
-            order.CreatedAt = DateTime.UtcNow;
-            await _orderRepo.AddAsync(order);
-            string cacheKey = $"order:{order.Id}";
-            await _cache.SetAsync(cacheKey, order, TimeSpan.FromMinutes(5));
+            try
+            {
+                var order = _mapper.Map<Order>(orderDto);
+                order.Id = Guid.NewGuid();
+                order.CreatedAt = DateTime.UtcNow;
+                await _orderRepo.AddAsync(order);
+                _logger.LogInformation("Order Created");
+                string cacheKey = $"order:{order.Id}";
+                await _cache.SetAsync(cacheKey, order, TimeSpan.FromMinutes(5));
+            }
+            catch (Exception ex) 
+            {
+                _logger.LogError(ex, "Error while Creating order");
+                throw;
+            }
         }
         public async Task DeleteOrderAsync(Guid id)
         {
-            var order = await _orderRepo.GetByIdAsync(id);
-            if (order != null)
+            try
             {
+                var order = await _orderRepo.GetByIdAsync(id);
+                if (order == null)
+                {
+                    _logger.LogWarning("Order Not Found");
+                    return;
+                }
                 await _orderRepo.DeleteAsync(order);
                 await _cache.RemoveAsync($"order:{id}");
+                _logger.LogInformation("Order Deleted Successfully");
             }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while Deleting order");
+                throw;
+            }
+            
         }
 
         public async Task<List<OrderDto>> GetAllOrdersAsync()
         {
-            var orders = await _orderRepo.GetAllAsync();
-            return _mapper.Map<List<OrderDto>>(orders);
+            try
+            {
+                var orders = await _orderRepo.GetAllAsync();
+                _logger.LogInformation("Feteched all orders");
+                return _mapper.Map<List<OrderDto>>(orders);
+            }
+            catch (Exception ex) 
+            { 
+                _logger.LogError(ex,"Error While fetching orders");
+                throw;
+            } 
+
         }
 
         public async Task<OrderDto> GetOrderByIdAsync(Guid id)
         {
-            string cacheKey = $"order:{id}";
-
-            var cachedOrder = await _cache.GetAsync<Order>(cacheKey);
-            if (cachedOrder != null)
-                return _mapper.Map<OrderDto>(cachedOrder);
-
-            var order = await _orderRepo.GetByIdAsync(id);
-            if (order != null)
+            try
             {
-                await _cache.SetAsync(cacheKey, order, TimeSpan.FromMinutes(5));
+                string cacheKey = $"order:{id}";
+
+                var cachedOrder = await _cache.GetAsync<Order>(cacheKey);
+                if (cachedOrder != null)
+                {
+                    _logger.LogInformation("Order Fetched from cache");
+                    return _mapper.Map<OrderDto>(cachedOrder);
+                }
+
+                var order = await _orderRepo.GetByIdAsync(id);
+
+                if (order != null)
+                {
+                    await _cache.SetAsync(cacheKey, order, TimeSpan.FromMinutes(5));
+                    _logger.LogInformation("Order Fetched");                  
+                }
                 return _mapper.Map<OrderDto>(order);
             }
-
-            return null; 
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error While fetching");
+                throw;
+            }
+            
         }
     }
 }
